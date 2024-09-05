c5a11988e2a27704cae2d54c22139a7e876e4dbd
master <starodubcev@gmail.com>
2024-09-05 05:33:34 +0000

    add styles and logic from cyber metagraph

--- .github/scripts/modify_html/add_analytics.sh
+++ .github/scripts/modify_html/add_analytics.sh
@@ -2,7 +2,7 @@
 
 analytics_script="<script>
     const script = document.createElement('script');
-    script.src = 'https://metrics.cyb.ai/js/script.hash.js';
+    script.src = 'https://cv.land/js/script.hash.js';
     script.setAttribute('data-domain', location.host);
     script.defer = true;
     document.body.appendChild(script);
@@ -13,5 +13,3 @@ file="$GITHUB_WORKSPACE/build/index.html"
 sed -i "/<\/body>/i\\
 $(echo "$analytics_script" | sed 's/$/\\n/' | tr -d '\n')\\
 " "$file"
-
-
